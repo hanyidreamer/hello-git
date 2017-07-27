2e0504148feab57e80f7a2caa5bbb0002e0ff5e6
zhanghanyi5 <zhanghanyi5@crc.com.hk>
2017-07-27 10:38:51 +0800

    包装类

--- src/main/java/com/itoche/javapractice/PackageType.java
+++ src/main/java/com/itoche/javapractice/PackageType.java
@@ -4,8 +4,8 @@ public class PackageType
 {
 	public static void main(String[] args)
 	{
-		Integer aInteger = new Integer(1);
-		Integer bInteger = new Integer(1);
+		Integer aInteger = Integer.valueOf(1);
+		Integer bInteger = Integer.valueOf(1);
 		System.out.println(aInteger == bInteger);
 		System.out.println(aInteger.equals(bInteger));
 		System.out.println(aInteger.intValue() == bInteger.intValue());
